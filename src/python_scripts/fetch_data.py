ac8078f0f711ca8b10d7d3692dccd96e1c6c637e
Kevin Hoang <hello@kev1n.co>
2026-01-09 19:33:06 +0100

    Update fetch_data.py

--- src/python_scripts/fetch_data.py
+++ src/python_scripts/fetch_data.py
@@ -9,7 +9,10 @@ data_path.mkdir(parents=True, exist_ok=True)
 
 fred = Fred(os.getenv("FRED_API_KEY"))
 
-yf_ticker= {
+START_DATE = "2015-01-01"
+END_DATE = "2025-12-31"
+
+yf_ticker = {
     "btc": "BTC-USD",
     "eth": "ETH-USD",
     "dow": "^DJI",
@@ -18,9 +21,20 @@ yf_ticker= {
 }
 
 for key, ticker in yf_ticker.items():
-    df = yf.download(ticker, start="2015-01-01", end="2025-01-01")
+    df = yf.download(
+        ticker,
+        start=START_DATE,
+        end=END_DATE,
+        auto_adjust=False,
+        progress=False
+    )
+
+    df = df[["Close"]]
+    df.columns = [key]
+    df.index.name = "Date"
     df.to_csv(data_path / f"{key}_data.csv")
     print(f"Saved {key} data.")
+
 
 fred_vars = {
     "cpi": "CPIAUCSL",
@@ -29,10 +43,14 @@ fred_vars = {
     "usdindex": "DTWEXBGS"
 }
 
+start_date = pd.to_datetime(START_DATE)
+end_date = pd.to_datetime(END_DATE)
+
 for key, series_id in fred_vars.items():
     data = fred.get_series(series_id)
     df = pd.DataFrame(data, columns=[key])
     df.index = pd.to_datetime(df.index)
+    df = df[(df.index >= start_date) & (df.index <= end_date)]
+    df.index.name = "Date"
     df.to_csv(data_path / f"{key}_data.csv")
     print(f"Saved {key} data.")
-
